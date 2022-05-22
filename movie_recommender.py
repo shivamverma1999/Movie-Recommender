acae7197c78fd6bb01fe14c5a0c43309c2543519
shivamverma1999 <33162868+shivamverma1999@users.noreply.github.com>
2022-05-22 19:15:28 +0530

    Add files via upload

---
+++ movie_recommender.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+###### helper functions. Use them when needed #######
+def get_title_from_index(index):
+	return df[df.index == index]["title"].values[0]
+
+def get_index_from_title(title):
+	return df[df.title == title]["index"].values[0]
+##################################################
